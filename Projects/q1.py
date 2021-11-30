ee047dc94a6c8bb6f3417a0c9709ff43d83fb141
Shakil <tayefehshakila@gmail.com>
2021-11-30 15:48:54 +0100

    Add files via upload

---
+++ Projects/q1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 20 21:16:58 2017
+
+@author: shakil
+"""
+
+a=int(input("Enter the first number"))
+b=int(input("Enter the second number"))
+c=a**b
+print("%s powered by %s equals to :"%(a,b),c)
