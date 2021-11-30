00c45545e4e4495f4c223e03e80d84058bc630e3
Shakil <tayefehshakila@gmail.com>
2021-11-30 15:45:18 +0100

    Create a1.py

---
+++ Projects/Array/Python/a1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 13 08:35:18 2017
+
+@author: shakil
+"""
+
+l=[]
+print("Enter the elements")
+for a in range(0,20):
+    b=int(input())
+    l.append(b)
+l.reverse()
+print(l)
