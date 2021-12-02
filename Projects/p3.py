e2a4f2596c8964be39d541117c3b81dd480bdc73
Shakil <tayefehshakila@gmail.com>
2021-12-02 14:38:52 +0100

    Update p3.py

--- Projects/p3.py
+++ Projects/p3.py
@@ -5,8 +5,9 @@ Created on Wed Aug 23 10:12:35 2017
 @author: shakil
 """
 
+#I am not sure why I developed this code
 import math
-l=[23,41,69,123,137,247,411,943,2829,3151,5617,9453,16851,129191,387573]
+l=[23, 41, 69, 123, 137, 247, 411, 943, 2829, 3151, 5617, 9453, 16851, 129191, 387573]
 for a in l:
     b=int(math.sqrt(a))
     for c in range(3,b):
