b9cbbc7770f62088f7dd57ceb832b82bde2a9738
Shakil <tayefehshakila@gmail.com>
2021-12-02 14:30:32 +0100

    Update p1.py

--- Projects/p1.py
+++ Projects/p1.py
@@ -5,7 +5,8 @@ Created on Fri May 26 07:11:00 2017
 @author: shakil
 """
 
-#this code computes the sum of all numbers between 1-1000 that are dividable by 3 and 5
+#this code is a solution to the [first problem of projecteuler.net](https://projecteuler.net/problem=1)
+#it calculates the sum of all the multiples of 3 or 5 below 1000
 sum=0
 for a in range(1,1000):
     if(a%3==0 or a%5==0):
