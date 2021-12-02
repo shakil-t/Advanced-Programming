d08912b4024b720b1b72a5fd9659502701e451b4
Shakil <tayefehshakila@gmail.com>
2021-12-02 14:20:01 +0100

    Update p1.py

--- Projects/p1.py
+++ Projects/p1.py
@@ -5,6 +5,7 @@ Created on Fri May 26 07:11:00 2017
 @author: shakil
 """
 
+#this code computes the sum of all numbers between 1-1000 that are dividable by 3 and 5
 sum=0
 for a in range(1,1000):
     if(a%3==0 or a%5==0):
