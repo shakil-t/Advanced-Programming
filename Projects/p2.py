6e582da918c4194a6f6b7e329d48415506a92d03
Shakil <tayefehshakila@gmail.com>
2021-12-02 14:27:26 +0100

    Update p2.py

--- Projects/p2.py
+++ Projects/p2.py
@@ -5,6 +5,8 @@ Created on Fri May 26 07:21:55 2017
 @author: shakil
 """
 
+#this code is a solution to the [second problem of projecteuler.net](https://projecteuler.net/problem=2)
+#it calculates the sum of even-valued terms in the Fibonacci sequence
 n1=1
 n2=2
 n3=0
