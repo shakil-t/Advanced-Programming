66092f91f080412aed083216623d336fb0e92b41
Shakil <tayefehshakila@gmail.com>
2021-12-03 12:40:16 +0100

    Add files via upload

---
+++ Projects/Count/New Count.java
@@ -0,0 +1,41 @@
+package count;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class count {
+
+    public static void main(String[] args) throws IOException {
+        List<String> words = new ArrayList<String>();
+        List<Integer> counts = new ArrayList<Integer>();
+        File file = new File("C:/Users/shakil/count.txt");
+
+        try (Scanner sc = new Scanner(new BufferedReader(new FileReader(file)))) {
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine().toLowerCase();
+                if (line.equals("")) {
+                    continue;
+                }
+                for (String word : line.split(" ")) {
+                    if (words.contains(word)) {
+                        int index = words.indexOf(word);
+                        int count = counts.get(index) + 1;
+                        counts.set(index, count);
+                    } else { //new word
+                        words.add(word);
+                        counts.add(1);
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < words.size(); i++) {
+            System.out.println(words.get(i) + ": " + counts.get(i));
+        }
+
+    }
+}
